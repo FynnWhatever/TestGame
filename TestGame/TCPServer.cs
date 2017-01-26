6ea8e5ca72f88896ecc7e094b5c9eb3a0ffe251f
FynnWhatever <fynn.whatever@gmail.com>
2017-01-26 20:43:31 +0100

    UDP Support

--- TestGame/TCPServer.cs
+++ TestGame/TCPServer.cs
@@ -13,6 +13,7 @@ namespace TestGame
         private TcpListener _server;
         private Boolean _isRunning;
         private Game1 game;
+        int port;
 
         public TCPServer(int port, Game1 g1)
         {
@@ -20,6 +21,7 @@ namespace TestGame
             _server = new TcpListener(IPAddress.Any, port);
             _server.Start();
             _isRunning = true;
+            this.port = port;
         }
 
         public void LoopClients()
@@ -34,10 +36,10 @@ namespace TestGame
 
         public void HandleClient(object obj)
         {
-            
+
             TcpClient client = (TcpClient)obj;
             NetworkStream nStream = client.GetStream();
-
+            string ip = ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString();
             Boolean bClientConnected = true;
 
 
@@ -52,11 +54,19 @@ namespace TestGame
             nStream.Read(rcvBytesID, 0, rcvLenID);
             String id = System.Text.Encoding.ASCII.GetString(rcvBytesID);
 
+            ThreadStart starter = delegate { RecieveUDP(id, ip); };
+            Thread thread = new Thread(starter);
+            thread.Start();
+
             game.CreatePlayer(id);
 
             while (bClientConnected)
             {
-                
+                /*
+                 *Do some TCP HANDLING here 
+                */
+
+                /*
                 byte[] rcvLenBytes = new byte[4];
                 nStream.Read(rcvLenBytes,0,4);
                 
@@ -67,10 +77,9 @@ namespace TestGame
                 byte[] rcvBytes = new byte[rcvLen];
                 nStream.Read(rcvBytes,0,rcvLen);
                 String rcv = System.Text.Encoding.ASCII.GetString(rcvBytes);
-
-                PlayerInput input = new System.Web.Script.Serialization.JavaScriptSerializer().Deserialize<PlayerInput>(rcv);
                 if (rcv.Equals(""))
                     break;
+                PlayerInput input = new System.Web.Script.Serialization.JavaScriptSerializer().Deserialize<PlayerInput>(rcv);
                 if (input == null)
                     break;
                 
@@ -78,10 +87,35 @@ namespace TestGame
                 game.MovePlayer(id,input.x,input.y);
                 input = null;
 
+                */
+
             }
             game.DestroyPlayer(id);
             nStream.Close();
             client.Close();
+        }
+
+        void RecieveUDP(string id, string ip)
+        {
+            UdpClient receivingUdpClient = new UdpClient(port);
+            IPAddress udpAddress = IPAddress.Parse(ip);
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(udpAddress, port);
+
+            Boolean bClientConnected = true;
+
+            while (bClientConnected)
+            {
+                Byte[] rcvBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+                String rcv = System.Text.Encoding.ASCII.GetString(rcvBytes);
+                if (rcv.Equals(""))
+                    break;
+                PlayerInput input = new System.Web.Script.Serialization.JavaScriptSerializer().Deserialize<PlayerInput>(rcv);
+                if (input == null)
+                    break;
+
+                game.MovePlayer(id, input.x, input.y);
+                input = null;
+            }
         }
     }
 }
